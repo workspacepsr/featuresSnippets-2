a9cb168903a77de98e0d7d9d06e33d246658c893
workspacepsr <78226717+workspacepsr@users.noreply.github.com>
2021-03-17 07:49:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+  notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener('click', () => addNewNote())
 console.log();
 
@@ -27,6 +33,8 @@ function addNewNote(text = '') {
 
   deleteBtn.addEventListener('click', () => {
     note.remove()
+
+    updateLS()
   })
 
   editBtn.addEventListener('click', () => {
@@ -38,7 +46,19 @@ function addNewNote(text = '') {
     const { value } = e.target
 
     main.innerHTML = marked(value)
+
+    updateLS()
   })
 
   document.body.appendChild(note)
+}
+
+function updateLS() {
+  const notesText = document.querySelectorAll('textarea')
+
+  const notes = []
+
+  notesText.forEach(note => notes.push(note.value))
+
+  localStorage.setItem('notes', JSON.stringify(notes))
 }
