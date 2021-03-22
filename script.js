fc8262f2c815d867314dc934dc8668197742e369
workspacepsr <78226717+workspacepsr@users.noreply.github.com>
2021-03-22 22:31:10 +0530

    update

--- script.js
+++ script.js
@@ -1,64 +1,44 @@
-const addBtn = document.getElementById('add')
+const draggables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".container");
 
-const notes = JSON.parse(localStorage.getItem('notes'));
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
 
-if(notes) {
-  notes.forEach(note => addNewNote(note))
-}
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
+  });
+});
 
-addBtn.addEventListener('click', () => addNewNote())
-console.log();
+containers.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+});
 
-function addNewNote(text = '') {
-  const note = document.createElement('div')
-  note.classList.add('note')
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ];
 
-  note.innerHTML = `
-  <div class="tools">
-      <button class="edit"><i class="fas fa-edit"></i></button>
-      <button class="delete"><i class="fas fa-trash-alt"></i></button>
-  </div>
-
-  <div class="main ${text ? "" : "hidden"}"></div>
-  <textarea> cla ss="${text ? "hidden" : ""}"</textarea>
-  `
-
-  const editBtn = note.querySelector('.edit')
-  const deleteBtn = note.querySelector('.delete')
-  const main = note.querySelector('.main')
-  const textArea = note.querySelector("textarea")
-
-  textArea.value = text
-  main.innerHTML = marked(text)
-
-  deleteBtn.addEventListener('click', () => {
-    note.remove()
-
-    updateLS()
-  })
-
-  editBtn.addEventListener('click', () => {
-    main.classList.toggle('hidden')
-    textArea.classList.toggle('hidden')
-  })
-
-  textArea.addEventListener('input', (e) => {
-    const { value } = e.target
-
-    main.innerHTML = marked(value)
-
-    updateLS()
-  })
-
-  document.body.appendChild(note)
-}
-
-function updateLS() {
-  const notesText = document.querySelectorAll('textarea')
-
-  const notes = []
-
-  notesText.forEach(note => notes.push(note.value))
-
-  localStorage.setItem('notes', JSON.stringify(notes))
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
